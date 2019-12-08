ed1d0678d2958c4fd7de68b07818e9f879d7ab34
anddro <anddro@mail.ru>
2019-12-08 21:38:58 +0300

    Скетч для тестирования IR

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,3 +1,4 @@
+
 // пример работы с лентой
 #define LED_PIN 13       // пин ленты
 #define NUMLEDS_TREE 100      // кол-во светодиодов на елке
@@ -13,14 +14,20 @@
 // но уменьшится и количество оттенков и уровней яркости!
 
 // ВНИМАНИЕ! define настройки (ORDER_GRB и COLOR_DEBTH) делаются до подключения библиотеки!
-#include <microLED.h>
+#include <microLED.h> // https://github.com/AlexGyver/GyverLibs/tree/master/microLED
+
+#include <IRremote.h> // https://github.com/z3t0/Arduino-IRremote
 
 LEDdata leds[NUMLEDS];  // буфер ленты типа LEDdata (размер зависит от COLOR_DEBTH)
 microLED strip(leds, NUMLEDS, LED_PIN);  // объект лента
 
+int RECV_PIN = 11;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
 void setup() {
-  //Serial.begin(9600);
-  //Serial.println("start");
+  Serial.begin(9600);
+  Serial.println("start");
 
   strip.setBrightness(200);    // яркость (0-255)
   // яркость применяется при выводе .show() !
@@ -29,6 +36,8 @@ void setup() {
   
   treeInit();
   
+  irrecv.enableIRIn(); // Start the receiver
+
   randomSeed(millis());
 }
 
@@ -42,4 +51,9 @@ void loop() {
 
     strip.show();
     delay(10);
+
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    irrecv.resume(); // Receive the next value
+  }
 }
